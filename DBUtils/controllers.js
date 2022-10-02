c6c62a464fdb556b34a004464c4e8378649b5a68
sadiq <link2nuhu@gmail.com>
2022-10-02 01:15:34 +0100

    my first commit to the backend repo

---
+++ DBUtils/controllers.js
@@ -0,0 +1,254 @@
+const mongo = require('mongodb')
+const url = 'mongodb://localhost:27017'
+getAll = (req, res) =>{
+   var query
+    mongo.MongoClient.connect(url, (err, connection)=>{
+        if(err){
+            console.log(err)
+        }else{
+            const user = req.body.userDetail.userType
+            if(user=="user"){
+                query = { customerManager: req.body.userDetail.email }
+            }
+            if(user=="admin"){
+                query={}
+            }
+            console.log(query)
+            const db = connection.db("Sizer")
+            db.collection("customers").find(query).toArray((err, result)=>{
+                if(err){
+                    console.warn(err)
+                }else{
+                    console.log(result)
+                    res.status(200).json({
+                        message:result
+                       })
+                       connection.close()
+                }
+            })
+        }
+    })
+}
+saveMeasurement = (req, res) =>{
+    mongo.MongoClient.connect(url, (err, connection)=>{
+        if(err){
+            console.log(err)
+        }else{
+            var measurement={}
+            measurement.kaftan = req.body
+            const db = connection.db("Sizer")
+            db.collection("customers").
+           updateOne({phone:req.body.owner},{$set:{measurement:measurement}},(err, result)=>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                res.status(200).json({
+                    message:"measurement saved successfully"
+                })
+            }
+            
+           })
+        }
+    })
+}
+
+updateMeasurement = (req, res) =>{
+    mongo.MongoClient.connect(url, (err, connection)=>{
+        if(err){
+            console.log(err)
+        }else{
+            var measurement={}
+            measurement.kaftan = req.body
+            const db = connection.db("Sizer")
+            db.collection("customers").
+           updateOne({phone:req.body.owner},{$set:{measurement:measurement}},(err, result)=>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                res.status(200).json({
+                    message:"measurement saved successfully"
+                })
+            }
+            
+           })
+        }
+    })
+}
+
+signUp = (req, res) =>{
+    var approval
+    var userType
+    mongo.MongoClient.connect(url, (err, connection)=>{
+        if(err){
+            console.log(err)
+        }else{
+            const db = connection.db("Sizer")
+            db.collection("users")
+            .countDocuments({$or:[{email:req.body.email}, {phone:req.body.phone}]},(err, result)=>{
+                if(err){
+                    console.log(err)
+                }else{
+                    if(result == 0){
+                        db.collection("users")
+                        .countDocuments({}, (error, result)=>{
+                            if(error){
+                                console.log(error)
+                            }else{
+                                if(result > 0){
+                                    approval = "not approved"
+                                    userType = "user"
+                                    const body = req.body
+                                    body.approval = approval
+                                    body.userType = userType
+                                    db.collection("users")
+                                    .insertOne(body, (error, result)=>{
+                                        if(error){
+                                            res.status(500).json({
+                                                message:"srever error: " + error
+                                            })
+                                        }else{
+                                            res.status(201).json({
+                                                message:"user created successfully"
+                                            })
+                                        }
+                                    })
+                                }else{
+                                    approval = "approved"
+                                    userType="admin"
+                                    const body = req.body
+                                    body.approval = approval
+                                    body.userType = userType
+                                    db.collection("users")
+                                    .insertOne(body, (error, result)=>{
+                                        if(error){
+                                            res.status(500).json({
+                                                message:"server error: " + error
+                                            })
+                                        }else{
+                                            res.status(201).json({
+                                                message:"admin created successfully"
+                                            })
+                                        }
+                                    })
+                                }
+                            }
+                        })
+                    }else{
+                        res.status(409).json({
+                            message:"email or password already exist"
+                        })
+                    }
+                }
+            })
+            
+
+        }
+})
+}
+LogIn = (req, res) =>{
+    mongo.MongoClient.connect(url, (err, connection)=>{
+        if(err){
+            console.log(err)
+        }else{
+            const db = connection.db("Sizer")
+            db.collection("users")
+            .countDocuments({$and:[{email:req.body.email}, {password:req.body.password}]}, (error, result)=>{
+                if(error){
+                    res.status(500).json({
+                        message:"server error :" + error,
+                        status:500
+                    })
+                }
+                if(result > 0){
+                    db.collection("users")
+                    .findOne({email:req.body.email},(error, result)=>{
+                        if(error){
+                            console.warn(error)
+                        }
+                        else{
+                            if(result.userType == "admin"){
+                                db.collection("customers").find({}).toArray((error, cresult)=>{
+                                    if(error){
+                                        console.warn(error)
+                                    }
+                                    else{
+                                        res.status(200).json({
+                                            message:"login successful",
+                                            status:200,
+                                            userEmail:req.body.email,
+                                            userDetails:result,
+                                            customers:cresult
+                                        })
+                                    }
+                                })
+                            }
+                            else{
+                            res.status(200).json({
+                                message:"login successful",
+                                status:200,
+                                userEmail:req.body.email,
+                                userDetails:result
+                            })
+                        }
+                    }
+                    })
+                    
+                }else{
+                    res.status(203).json({
+                        message:"email or password invalid",
+                        status:203
+                    })
+                }
+            })
+        }
+    })
+}
+
+createCustomer = (req, res) =>{
+    mongo.MongoClient.connect(url, (err, connection)=>{
+        if(err){
+            console.log(err)
+        }else{
+            const db = connection.db("Sizer")
+            db.collection("customers")
+            .countDocuments({phone:req.body.phone},(err, result)=>{
+                if(err){
+                    console.log(err)
+                }else{
+                    if(result == 0){
+                                    db.collection("customers")
+                                    .insertOne(req.body, (error, result)=>{
+                                        if(error){
+                                            res.status(500).json({
+                                                message:"server error: " + error
+                                            })
+                                        }else{
+                                            res.status(201).json({
+                                                message:"customer created successfully",
+                                                status:201
+                                            })
+                                        }
+                                    })
+                    }else{
+                        res.status(409).json({
+                            message:"customer with this phone number already exist"
+                        })
+                    }
+                }
+            })
+            
+
+        }
+})
+}
+
+module.exports = {
+    getAll,
+    saveMeasurement,
+    signUp,
+    LogIn,
+    createCustomer,
+    updateMeasurement
+}
---
+++ index.js
@@ -0,0 +1,30 @@
+const cors = require('cors')
+const express = require('express')
+const bordyparser = require('body-parser')
+const controls = require('./DBUtils/controllers')
+const app = express()
+app.use(bordyparser.json())
+app.use(cors())
+app.post("/getall", (req, res)=>{
+     controls.getAll(req, res)
+})
+app.post("/saveMeasurement", (req, res)=>{
+    controls.saveMeasurement(req, res)
+})
+app.post("/updateMeasurement", (req, res)=>{
+    controls.saveMeasurement(req, res)
+})
+app.post("/signUp", (req, res)=>{
+    controls.signUp(req, res)
+})
+app.post("/createCustomer", (req, res)=>{
+    controls.createCustomer(req, res)
+})
+app.post("/LogIN", (req, res)=>{
+    controls.LogIn(req, res)
+})
+const portNumber = process.env.portNumber || 8082
+app.listen(portNumber,()=>{
+    console.log(`listening at port ${portNumber}`)
+})
+
